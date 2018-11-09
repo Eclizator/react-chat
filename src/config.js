ee25c7b674a2618fff7a296886d2b1cffeb9193b
Dima <eclizato@gmail.com>
2018-11-09 02:02:14 +0200

    and another one

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   API_URI: process.env.NODE_ENV === 'production'
-    ? 'https://doge-codes-chat-api.herokuapp.com/v1'
+    ? 'https://dogecodes-chat-api.herokuapp.com/v1'
     : 'http://localhost:8000/v1',
   SOCKETS_URI: process.env.NODE_ENV === 'production'
-    ? 'wss://doge-codes-chat-api.herokuapp.com/'
+    ? 'wss://dogecodes-chat-api.herokuapp.com/'
     : 'ws://localhost:8000/',
 };
